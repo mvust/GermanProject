a51490bdd725263930abd6f29528b34e90e63265
Martin Vust <martinvust@gmail.com>
2016-05-10 21:34:01 -0400

    3.1b
    Adding in unit tests

---
+++ test/GermanProject.UnitTests/AppControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GermanProject.Controllers;
+using Xunit;
+
+namespace GermanProject.UnitTests
+{
+    public class AppControllerTest
+    {
+        [Fact]
+        public void IsAuthorizedTest()
+        {
+
+        }
+    }
+}
---
+++ test/GermanProject.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GermanProject.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GermanProject.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e35e9069-ff1b-4aae-ae27-4c624389c000")]
